e68181e644c34625acdde403c1c14171af319474
HelenELee <HelenELee3@outlook.com>
2023-03-02 11:28:17 +0800

    added handlebars and js for adding a comment

--- public/js/comment.js
+++ public/js/comment.js
@@ -3,21 +3,27 @@ const commentFormHandler = async (event) => {
   
     // Collect values from the login form
     //const email = document.querySelector('#email-login').value.trim();
-    const comment = document.querySelector('#comment').value.trim();
-    console.log(comment);
-    if (comment) {
+    const contents = document.querySelector('#comment').value.trim();
+    console.log(contents);
+    if (contents) {
       // Send a POST request to the API endpoint
+      console.log("posting comment");
+
+      console.log(JSON.stringify({ contents, 
+        "user_id": "1",
+         "blog_id": "3"}),)
+
       const response = await fetch('/api/comments', {
         method: 'POST',
-        body: JSON.stringify({ comment, 
-                user_id: 1,
-                blog_id: 1}),
+        body: JSON.stringify({ contents, 
+               "user_id": "1",
+                "blog_id": "3"}),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
         // If successful, redirect the browser to the home page
-        document.location.replace('/');
+        document.location.replace('/api/blogs/3');
       } else {
         alert(response.statusText);
       }
