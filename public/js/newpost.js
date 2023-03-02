dbad8ef471dc06aaaffa95507abc1697c1296f8c
HelenELee <HelenELee3@outlook.com>
2023-03-02 11:24:38 +0800

    js and handlebars for posting a new blog post

---
+++ public/js/newpost.js
@@ -0,0 +1,28 @@
+const postFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // Collect values from the login form
+    const title = document.querySelector('#blog-title').value.trim();
+    const contents = document.querySelector('#blog-contents').value.trim();
+    //alert(req.session.user_id);
+    if (title && contents) {
+      // Send a POST request to the API endpoint
+      //TO DO Get actual user_id
+      const response = await fetch('/api/blogs', {
+        method: 'POST',
+        body: JSON.stringify({ title, contents}),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+
+  document
+  .querySelector('#post-blog')
+  .addEventListener('click', postFormHandler);
